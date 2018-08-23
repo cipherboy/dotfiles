e8896ed6087b8f5743dce74c26b789adc88a0d10
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-08-23 18:04:16 -0400

    Updates to git dotfiles
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -1,9 +1,10 @@
 # git aliases
 alias gta='git add'
 alias gtb='git branch'
-alias gtbn='basename `gtcd`'
+alias gtbn='basename "$(gtcd)"'
 alias gtc='git clone'
 function gtcd() {
+    # Changes to the root of the git repository
     local git_root="$(git rev-parse --show-toplevel 2>/dev/null)"
     pushd "$git_root" 1>/dev/null 2>&2
     echo "$git_root"
@@ -36,6 +37,7 @@ alias gtsl='git shortlog -s -n'
 alias gtu='git pull'
 
 function gtbs() {
+    # Skips current commit in bisect
     local git_root="$(git rev-parse --show-toplevel 2>/dev/null)"
     pushd $git_root >/dev/null 2>/dev/null
         git bisect skip
@@ -43,6 +45,7 @@ function gtbs() {
 }
 
 function gtbg() {
+    # Marks current commit as good in bisect
     local git_root="$(git rev-parse --show-toplevel 2>/dev/null)"
     pushd $git_root >/dev/null 2>/dev/null
         git bisect good
@@ -50,6 +53,7 @@ function gtbg() {
 }
 
 function gtbb() {
+    # Marks current commit as bad in bisect
     local git_root="$(git rev-parse --show-toplevel 2>/dev/null)"
     pushd $git_root >/dev/null 2>/dev/null
         git bisect bad
@@ -57,6 +61,8 @@ function gtbb() {
 }
 
 function gtcpb() {
+    # For given branch ($1), cherry-picks the remaining arguments to
+    # that branch.
     local current="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
     local branch=$1
     shift
@@ -68,28 +74,72 @@ function gtcpb() {
 }
 
 function gtuf() {
+    # Force updating the branch
     local branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
-    git pull origin "$branch"
+    git fetch origin "$branch"
     git reset --hard "origin/$branch"
     git status
 }
 
+function gthr() {
+    # Checks if repository has a remote named $1.
+    local remote="$1"
+    local result="$(git remote 2>/dev/null | grep -o "^$remote$")"
+
+    if [ "x$result" == "x$remote" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
 function gtum() {
-    local branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
-    git checkout master && git pull && git pull origin master && git pull upstream master && git push
-    git checkout "$branch"
-    git status
+    # Updates the master branch
+    gtub master
 }
 
 function gtub() {
-    local current="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
-    local branch=$1
-    git checkout "$branch" && git pull && git pull origin $branch && git pull upstream "$branch" && git push
-    git checkout "$current"
+    # Fully updates the remote branch $1, syncing with upstream if it exists
+
+    local local_branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+    local remote_branch="$1"
+    if [ "$remote_branch" == "x" ]; then
+        remote_branch="master"
+    fi
+
+    git checkout "$remote_branch"
+    local ret="$?"
+    if (( ret != 0 )); then
+        return $ret
+    fi
+
+    git fetch --all
+    local ret="$?"
+    if (( ret != 0 )); then
+        return $ret
+    fi
+
+    git pull origin "$remote_branch"
+
+    gthr "upstream"
+    ret="$?"
+    if (( ret == 0 )); then
+        git pull upstream "$remote_branch"
+        git reset --hard "upstream/$remote_branch"
+    fi
+
+    git push
+    git checkout "$local_branch"
     git status
 }
 
 function ghcd() {
+    # Change to a given repository if it exists.
+    #
+    # Usage:
+    # ghcd hash_framework -> changes to "$HOME/GitHub/cipherboy/hash_framework"
+    # ghcd c2 hash_framework -> changes to "$HOME/cipherboy/c2/hash_framework"
+
     local gitbase="$HOME"
     local provider="GitHub"
     local username="cipherboy"
@@ -132,6 +182,8 @@ function ghcd() {
 }
 
 function ghr() {
+    # Opens a branch for review
+
     local owner="$1"
     local project="$2"
     local branch="$3"
@@ -147,15 +199,31 @@ function ghr() {
     fi
 
     cd "$project"
+    local ret="$?"
+    if (( ret != 0 )); then
+        return $ret
+    fi
+
     git checkout master
     git pull origin
     git checkout "$branch"
-    git pull origin "$branch"
-    git reset --hard "origin/$branch"
+    ret="$?"
+    if (( ret != 0 )); then
+        return $ret
+    fi
+
+    # Do a force update of this branch
+    gtuf
+    ret="$?"
+    if (( ret != 0 )); then
+        return $ret
+    fi
+
     build all
 }
 
 function ghl() {
+    # Create a link to a given file in a smart fashion
     local path="$1"
     local line="$2"
     local branch="master"
@@ -185,6 +253,8 @@ function ghl() {
 }
 
 function gtpoffline() {
+    # Do an offline push of this repository
+
     local git_root="$(git rev-parse --show-toplevel 2>/dev/null)"
     echo "$git_root" >> ~/.git_offline_push
     sort -u ~/.git_offline_push > ~/.git_offline_push.tmp
@@ -192,6 +262,8 @@ function gtpoffline() {
 }
 
 function gtponline() {
+    # Push all repositories that were pushed when offline.
+
     cp ~/.git_offline_push ~/.git_offline_push.original
     local line="$(head -n 1 ~/.git_offline_push)"
     while [ "x$line" != "x" ]; do
@@ -206,6 +278,8 @@ function gtponline() {
 }
 
 function gtse() {
+    # Set email for the repository to personal or work.
+
     local email="$1"
     if [ "x$email" == "xwork" ] || [ "x$email" == "xw" ]; then
         email="$(cat ~/.git_email_work)"
@@ -216,6 +290,8 @@ function gtse() {
 }
 
 function gtseg() {
+    # Set email globally to personal or work.
+
     local email="$1"
     if [ "x$email" == "xwork" ]; then
         email="$(cat ~/.git_email_work)"
@@ -226,6 +302,8 @@ function gtseg() {
 }
 
 function gtrsp() {
+    # Rebase, resetting author and signoff
+
     local current="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
     git rebase -i "origin/$current"
     ret=$?
@@ -237,6 +315,8 @@ function gtrsp() {
 }
 
 function gtbac() {
+    # Rebase, building and testing commits using `build all`
+
     local base="$1"
     if [ "x$base" == "x" ]; then
         base="--fork-point"
