3cceb2493011058df6a0918ea321f1b78c465b22
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-10-20 10:39:54 -0400

    Add shorter aliases
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ bashrc.d/shorteners.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Python
+alias p2="python2"
+alias p3="python3"
+
+# SSH
+alias sr7="ssh recon7"
+alias sr7r="ssh recon7r"
+alias pr7="sftp recon7"
+alias pr7r="sftp recon7r"
+
+function sirc() {
+    local host="$1"
+    if [ "x$host" = "x" ]; then
+        host="cipherboy"
+    fi
+
+    ssh "chat@$host" -t 'screen -r -d'
+}
+
+# vim
+alias v="vim"
