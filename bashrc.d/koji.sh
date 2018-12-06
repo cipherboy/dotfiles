f3e0f699268cfebeeb3478cd73b199136fc8ac0e
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-12-06 16:29:37 -0500

    Don't write URLs to file by default
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- bashrc.d/koji.sh
+++ bashrc.d/koji.sh
@@ -3,5 +3,5 @@
 function koji_rpms() {
     local url="$1"
     local arch="$2"
-    wget -O- "$url" | grep '\.rpm' | grep -o 'href="[^"]*\.rpm"' | sed 's/^href="//g' | sed 's/"$//g' | grep "$arch" > /tmp/urls
+    curl -s "$url" | grep '\.rpm' | grep -o 'href="[^"]*\.rpm"' | sed 's/^href="//g' | sed 's/"$//g' | grep "$arch"
 }
