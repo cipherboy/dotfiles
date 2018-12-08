22aaa12b5f93c3e35a13bb688f21edeb09c8670e
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-12-08 09:48:44 -0500

    Add GitHub module with gh_pr_commits
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ bashrc.d/github.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function gh_pr_commits() {
+    local user="$1"
+    local repo="$2"
+    local pull="$3"
+
+    local commits="$(curl -s https://api.github.com/repos/$user/$repo/pulls/$pull/commits 2>/dev/null | jq length)"
+    echo "$user/$repo pr#$pull has $commits commits"
+
+    local commit_sha="$(curl -s https://api.github.com/repos/$user/$repo/pulls/$pull | jq '.["merge_commit_sha"]' | sed 's/"//g')"
+    for i in $(seq 1 $commits); do
+        echo "$commit_sha"
+
+        local commit_info="$(curl -s https://api.github.com/repos/$user/$repo/git/commits/$commit_sha)"
+        local parent_count="$(echo "$commit_info" | jq '.["parents"]' | jq length)"
+        if [ "x$parent_count" != "x1" ]; then
+            echo "Can't process commit $commit_sha: merge commit with multiple parents."
+            return 1
+        fi
+        commit_sha="$(echo "$commit_info" | jq '.["parents"][0]["sha"]' | sed 's/"//g')"
+    done
+
+    return 0
+}
