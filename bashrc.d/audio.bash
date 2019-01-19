90ecd8cd59ccab4a2d5394ad81c4505992ddab8f
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-01-19 12:05:07 -0500

    Add FLAC transcoding
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- bashrc.d/audio.bash
+++ bashrc.d/audio.bash
@@ -6,3 +6,7 @@ alias flac2mp3='parallel avconv -i {} -qscale:a 320k -b 320k {.}.mp3 ::: *.flac'
 alias m4a2mp3='parallel avconv -i {} -qscale:a 320k -b 320k {.}.mp3 ::: *.m4a'
 alias wav2mp3='parallel avconv -i {} -qscale:a 320k -b 320k {.}.mp3 ::: *.wav'
 
+# Transcode to FLAC
+alias m4a2flac='parallel ffmpeg -i {} {.}.flac ::: *.m4a'
+alias wav2flac='parallel ffmpeg -i {} {.}.flac ::: *.wav'
+
