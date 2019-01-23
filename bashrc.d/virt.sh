63c6d441f1d27dbbd43aa962935e6a29f292aad7
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-01-23 15:18:50 -0500

    Add vssh
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- bashrc.d/virt.sh
+++ bashrc.d/virt.sh
@@ -7,3 +7,22 @@ function vm_list() {
 function vm_addrs() {
     sudo virsh domifaddr --full "$1"
 }
+
+function vssh() {
+    local name="$(vm_list | grep "$1" | awk '{print $2}')"
+    local lines="$(wc -l <<< "$name")"
+    if (( lines != 1 )); then
+        echo "$name"
+        return 1
+    fi
+
+    local addr="$(vm_addrs "$name" | grep -o '192.168.122.[0-9]*' | head -n 1)"
+
+    local user="$2"
+    if [ "x$user" == "x" ]; then
+        user="root"
+    fi
+
+    ssh-copy-id "$user"@"$addr"
+    ssh "$user"@"$addr"
+}
