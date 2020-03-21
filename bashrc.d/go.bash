661225e8bbaadd0c5e4e76af0d6f26e6318ff26e
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-03-21 08:10:30 -0400

    [go] Started gcd
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- bashrc.d/go.bash
+++ bashrc.d/go.bash
@@ -2,3 +2,11 @@ export GOPATH="$HOME/go"
 mkdir -p "$GOPATH"
 
 export PATH="$GOPATH/bin:$PATH"
+
+function gcd() {
+	if [ -e "$GOPATH/src/git.cipherboy.com/"*"/$1" ]; then
+		pushd "$GOPATH/src/git.cipherboy.com/"*"/$1"
+	elif [ -e "$GOPATH/src/github.com/cipherboy/$1" ]; then
+		pushd "$GOPATH/src/github.com/cipherboy/$1"
+	fi
+}
