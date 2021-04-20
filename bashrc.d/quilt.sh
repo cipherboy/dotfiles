f49fb53f5c20f093fdaebe10162cb2d379e388b2
Alexander Scheel <alexander.m.scheel@gmail.com>
2021-04-20 13:02:26 -0400

    Add quilt patch directory auto-detection
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- bashrc.d/quilt.sh
+++ bashrc.d/quilt.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-alias qa='quilt applied'
-alias qi='quilt import'
-alias qs='quilt series'
+function qup() {
+    # Find absolute directory of patches in a debian/patches setup. Export
+    # it as QUILT_PATCHES=<dir> so that quilt can find it automagically.
+
+    if [ -e patches ]; then
+        export QUILT_PATCHES="$(pwd)/patches"
+    elif [ -e debian/patches ]; then
+        export QUILT_PATCHES="$(pwd)/debian/patches"
+    elif [ -e ../debian/patches ]; then
+        export QUILT_PATCHES="$(pwd)/../debian/patches"
+    elif [ -e ../../debian/patches ]; then
+        export QUILT_PATCHES="$(pwd)/../../debian/patches"
+    elif [ -e ../../../debian/patches ]; then
+        export QUILT_PATCHES="$(pwd)/../../../debian/patches"
+    elif [ -e ../../../../debian/patches ]; then
+        export QUILT_PATCHES="$(pwd)/../../../../debian/patches"
+    fi
+}
+
+alias qa='qup ; quilt applied'
+alias qi='qup ; quilt import'
+alias qs='qup ; quilt series'
 
 function qip() {
     local patch="$1"
@@ -10,6 +29,7 @@ function qip() {
         patch="$QUILT_RELATIVE/$patch"
     fi
 
+    qup
     quilt import "$patch" && quilt push
 }
 
