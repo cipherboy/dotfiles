7ff5f2c1adb00fea877c5bf254a25b8545a3a204
Alexander Scheel <alexander.m.scheel@gmail.com>
2021-04-23 13:32:03 -0400

    Add ery/eo for extracting yaml/oval
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- bashrc.d/ssg.bash
+++ bashrc.d/ssg.bash
@@ -8,3 +8,6 @@ function re() {
 export PYTHONPATH="$(gtcd):$PYTHONPATH"
 export PATH="$PATH:$(gtcd)/utils"
 export BUILD_CMAKE_ARGS=("-DSSG_PRODUCT_DEFAULT=OFF" "-DSSG_PRODUCT_UBUNTU2004=ON")
+
+alias ery='python3 ./tools/extract_rule_yml.py cisbenchmark/audit/Canonical_Ubuntu_20.04_CIS_Benchmark-xccdf.xml'
+alias eo='python3 ./tools/extract_oval.py cisbenchmark/audit/Canonical_Ubuntu_20.04_CIS_Benchmark-oval.xml'
