2a49637d3d937b514c64b6e2550b6cb52b28c4ff
Alexander Scheel <alex.scheel@canonical.com>
2021-04-29 16:36:48 -0400

    Add more SSG/CIS tooling
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

--- bashrc.d/ssg.bash
+++ bashrc.d/ssg.bash
@@ -14,6 +14,28 @@ function nr() {
     mkdir "$r" && vim "$r/rule.yml" && gta "$r" && gtm -m "Add $r for $2"
 }
 
+function rdir() {
+    # Change into a rule's directory.
+    local rule="$1"
+    local rule_yml="$(find "$(gtcd)" -type f | sed '/\/build\//d' | sed '/\/\.git\//d' | grep -i "$rule" | grep -i 'rule\.yml')"
+    local dir="$(dirname "$rule_yml")"
+    echo "$dir"
+    cd "$dir"
+}
+
+function cis() {
+    # Find CIS references in a rule.
+    local rule="$1"
+    local dir="$(rc "$rule")"
+    grep 'cis@ubuntu' "$dir/rule.yml"
+}
+
+function acis() {
+    # Find all files matching a specified CIS reference.
+    local identifier="$1"
+    grep -ir "cis@ubuntu.*$identifier" "$(gtcd)" | grep -i 'rule\.yml:'
+}
+
 export PYTHONPATH="$(gtcd):$PYTHONPATH"
 export PATH="$PATH:$(gtcd)/utils"
 export BUILD_CMAKE_ARGS=("-DSSG_PRODUCT_DEFAULT=OFF" "-DSSG_PRODUCT_UBUNTU2004=ON")
